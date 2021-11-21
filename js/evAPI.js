e1a12f15322407e2d4724ba33386c1f59b1891d2
LiesbethPoelmans <liesbeth.poelmans@gmail.com>
2021-11-21 18:09:04 +0100

    JS: cleanup + chargeTime added / HTML batterylevel field added

--- js/evAPI.js
+++ js/evAPI.js
@@ -32,7 +32,6 @@ function removeAllChildrenAndClean(el) {
     return;
 }
 
-
 ///// HTML => show Model in dropdown based on selection of model
 // Parameter: the make selected by the user
 function showEVdataModel(make) {
@@ -75,90 +74,7 @@ document.getElementById("make").addEventListener("change", function() {
     showEVdataModel(document.getElementById("make").value);
 });
 
-
-///// to be used in Accordion - charging calculation
-function showEVdataBattery(model) {
-    fetch('http://localhost:3000/cars').then(response => response.json()).then(data => {
-        const filteredBattery = data.filter((item) => {
-            return item.Model === model;
-        });
-        const mapBattery = filteredBattery.map((item) => {
-            return (item.Battery_capacity_kWh);///
-        });
-    console.log(mapBattery);
-});
-}
-
-document.getElementById("model").addEventListener("change", function() {
-    showEVdataBattery(document.getElementById("model").value);
-});
-
-///// to be used in Map calculation
-function showEVdataRange(model) {
-    fetch('http://localhost:3000/cars').then(response => response.json()).then(data => {
-        const filteredRange = data.filter((item) => {
-            return item.Model === model;
-        });
-        const mapRange = filteredRange.map((item) => {
-            return (item.Range_WLTP_km);
-        });
-    console.log(mapRange);
-});
-}
-
-document.getElementById("model").addEventListener("change", function() {
-    showEVdataRange(document.getElementById("model").value);
-});
-
-///// to be used in Accordion - charging calculation
-function showEVdataChargingPower(model) {
-fetch('http://localhost:3000/cars').then(response => response.json()).then(data => {
-        const filteredChargingPower = data.filter((item) => {
-            return item.Model === model;
-        });
-        const mapChargingPower = filteredChargingPower.map((item) => {
-            return (item.Maximum_DC_chargingPower_kW);
-        });
-    console.log(mapChargingPower);
-});
-}
-
-document.getElementById("model").addEventListener("change", function() {
-    showEVdataChargingPower(document.getElementById("model").value);
-});
-
-///// to be used in Accordion - charging calculation
-function showEVdataEnergyConsumption(model) {
-    fetch('http://localhost:3000/cars').then(response => response.json()).then(data => {
-        const filteredEnergyConsumption = data.filter((item) => {
-            return item.Model === model;
-        });
-        const mapEnergyConsumption = filteredEnergyConsumption.map((item) => {
-            return (item.mean_EnergyConsumption_kWh_per_100km);///
-        });
-        console.log(mapEnergyConsumption);
-
-        const distanceAB = 400; // From GoogleMaps API (To Do)
-
-        function calculateChargePrice () {
-            let distance = distanceAB; // From GoogleMaps API (Abdu - To Do)
-            let consumption = mapEnergyConsumption;
-            let price = 0.26; // from ChargePrice API => NA because data is not clean
-            document.getElementById("chargePrice").innerHTML = ((distance/100)*consumption)*price+` €`;
-            /*<span class="chargePrice" id="chargePrice"></span> To be added to HTML accordion*/        
-        }
-        console.log(calculateChargePrice());
-    });
-}
-
-document.getElementById("model").addEventListener("change", function() {
-    showEVdataEnergyConsumption(document.getElementById("model").value);
-});
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Alternative method, reduce amount of fetches by including all in one call
-
+//// Show Data based on selection Model
 function showTotalEVData(model) {
     fetch('http://localhost:3000/cars').then(response => response.json()).then(data => {
         const filteredEVdata = data.filter((item) => {
@@ -172,22 +88,55 @@ function showTotalEVData(model) {
             resultMap["Battery_capacity_kWh"] = item.Battery_capacity_kWh;
             return resultMap;
         });
+        calculateChargeTime(mapEVdata[0].Maximum_DC_chargingPower_kW, mapEVdata[0].Battery_capacity_kWh, mapEVdata[0].mean_EnergyConsumption_kWh_per_100km)
         console.log(mapEVdata);
+        calculateChargePrice(mapEVdata[0].mean_EnergyConsumption_kWh_per_100km);
     });
 }
 document.getElementById("model").addEventListener("change", function() {
     showTotalEVData(document.getElementById("model").value);
 });
 
+/* //////////////////// Calculation ChargeTime
+https://www.researchgate.net/post/How-to-count-the-charging-time-for-the-electric-vehicle
+Equation (charge time demand=Vehicle Battery Capacity (KWH)/ Charging Station Delivery Rate (KW)).
 
-/* //// Calculate chargeTime
-https://www.energuide.be/en/questions-answers/how-long-does-it-take-to-charge-an-electric-car-battery/1621/
+* battery capacity to be loaded = (capacity*level)-((distance/100)*consumption)
 
-1. First calculate your load power (P), by multiplying the voltage (U in volts) by the current (I, in amps). You get a value in watts.
-P = U x I => N/A, data not available
-For example: 16 A x 230 V = 3,680 W
-2. Divide the load power by 1,000 for a value in kilowatts.
-For example: 3,680 W = 3.7 kilowatts => From ChargePrice API
-3. Divide the power of your battery (also in kW) by the figure obtained to get the charging time.
-For example:  24 kW/ 3.7 kW= 6.5 hours => Battery Power vs Battery Capacity???? / kW vs kWh
+=> battery capacity to be loaded / load power.
+
+question: consumption * (range/100) should be equal to capacity => not the case in API
  */
+function calculateChargeTime(carchargingPower, battery, consumption) {
+    ////pick smallest chargingPower => car vs chargePoint
+    let chargePointChargingPower = 22; /// to be linked to priceCharge API
+    let chargingPower = (carchargingPower >= chargePointChargingPower) ? chargePointChargingPower : carchargingPower;
+
+    ////calculate charging time (float)
+    let distance = 300; // From GoogleMaps API (Abdu - To Do)
+    let level = 0.9; ////document.getElementById("batteryLevel").value does not work???;
+    let number = ((battery*level)-((distance/100)*consumption)) / chargingPower;
+
+    ////convert float to hh:mm
+    let sign = (number >= 0) ? 1 : -1;
+    number = number * sign;
+    let hour = Math.floor(number);
+    let decpart = number - hour;
+    let min = 1 / 60;
+    decpart = min * Math.round(decpart / min);
+    let minute = Math.floor(decpart * 60) + '';
+    if (minute.length < 2) {
+    minute = '0' + minute; 
+    }
+    sign = sign == 1 ? '' : '-';
+    time = sign + hour + ':' + minute;
+    document.getElementById("chargeTime").innerHTML = time
+}
+
+////calculate chargetime to be used in Accordion
+function calculateChargePrice (consumption) {
+    let distance = 400; // From GoogleMaps API (Abdu - To Do)            
+    let price = 0.26; // from ChargePrice API => NA because data is not clean
+    document.getElementById("chargePrice").innerHTML = Math.round(((((distance/100)*consumption)*price)+Number.EPSILON)*100)/100 +` €`;
+    //// based on level battery: (((range100%-rangelevel)+(distance/100)*consumption))*price+` €`;        
+}
